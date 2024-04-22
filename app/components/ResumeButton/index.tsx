18ca1ac646f2ed5717827faa866a0fb43e8bed84
Ossama Bilal <sheikhusamabilal@gmail.com>
2024-04-23 02:44:23 +0500

    comments removed

--- app/components/ResumeButton/index.tsx
+++ app/components/ResumeButton/index.tsx
@@ -8,11 +8,6 @@ const ResumeButton = () => {
       download="Ossama's resume"
       target="_blank"
     >
-      {/* <Button
-        className={`${fonts.chilanka} transition ease-in-out delay-150 px-5 cursor-pointer hover:bg-purple-400 hover:text-white`}
-      >
-        Resume
-      </Button> */}
       <Button
         className={`${fonts.chilanka} cursor-pointer pt-1 px-3 transition hover:-translate-x-1 hover:-translate-y-1 ease-in-out hover:shadow-[2px_2px_3px_3px_rgba(192,132,252)] text-white border-solid border-[1px] border-purple-400`}
       >
